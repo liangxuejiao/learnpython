754749a9cf9b7e4f2f11ee882a132f393ac4dba3
liangxuejiao <liangxuejiao163@163.com>
2018-12-03 17:08:14 +0800

    note

--- tuling.py
+++ tuling.py
@@ -20,22 +20,20 @@ raw_TULINURL = "http://www.tuling123.com/openapi/api?key=%s&info=" % API_KEY
 
 
 def result():
-    for i in range(1, 100):
-        queryStr = input("我:".encode('utf-8'))
+        queryStr = input("我：")
         TULINURL = "%s%s" % (raw_TULINURL, urllib.parse.quote(queryStr))
         req = urllib.request.Request(url=TULINURL)
-        result = urllib.request.urlopen(req).read()
-        hjson = json.loads(result)
-        length = len(hjson.keys())
-        content = hjson['text']
-
-        if length == 3:
+        result = urllib.request.urlopen(req).read()  # 用Request类构建了一个完整的请求，增加了headers等一些信息
+        hjson = json.loads(result)  # 请求之后的结果
+        length = len(hjson.keys())  # 判断dict属性个数
+        content = hjson['text']  # 获取dict中text的内容
+        if length == 3:  # 当回答中有额外添加url时，dict中会多加一个属性
             return 'robots:' + content + hjson['url']
-        elif length == 2:
+        elif length == 2:  # 回答中无url
             return 'robots:' + content
 
 
 if __name__ == '__main__':
     print ("你好，请输入内容:")
-    contents = result()
+    contents = result()  # 调用
     print (contents)
